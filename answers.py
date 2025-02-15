163e236e1169f2365be90e5026853470fadbcfb2
DmitriiG8421 <golikov.dima15@gmail.com>
2025-02-15 14:24:59 +1100

    I think I finished

--- answers.py
+++ answers.py
@@ -1,6 +1,17 @@
+#1. Object-Oriented Programming (OOP) is very popular model of coding and,
+#   used for software development and is regard as the standard way of coding.
+# Object-Oriented way of coding must have two concepts - Classes and objects.
+# Also, it has four pillars of OOP. The pillars include 
+
+# 1. Encapsulation 
+# 2. Abstraction 
+# 3. Inheritance 
+# 4. Polymorphism
+   
 
 
 #2. Class is a blueprint or a plan for an object.
+
 
 #3. We create a class bye writing "class" then the name of the class
 # and a column and finally the content of the class.
@@ -10,7 +21,12 @@ class House:
   doors = 3
   bathroom = 2.5
 
+
 #4. An object is an instant of a class. 
 #   The process of makiing an object is called instantiation
 
-#5. We crete an object by writing the name of the object
+
+#5. We crete an object by writing a variable and 
+# writing the name of the class with brackets
+#Example:
+Mansion = House()
