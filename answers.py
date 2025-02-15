b5a2b1b5df9061d22cbe95fe6a3c59b535ed9c7a
DmitriiG8421 <golikov.dima15@gmail.com>
2025-02-15 14:13:27 +1100

    code

---
+++ answers.py
@@ -0,0 +1,16 @@
+
+
+#2. Class is a blueprint or a plan for an object.
+
+#3. We create a class bye writing "class" then the name of the class
+# and a column and finally the content of the class.
+#Example:
+class House:
+  windows = 8
+  doors = 3
+  bathroom = 2.5
+
+#4. An object is an instant of a class. 
+#   The process of makiing an object is called instantiation
+
+#5. We crete an object by writing the name of the object
