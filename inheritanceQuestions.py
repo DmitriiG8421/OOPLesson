429b3f84a9d9fc6f0dfddfaa9cbc704f2425583c
DmitriiG8421 <golikov.dima15@gmail.com>
2025-03-08 14:10:21 +1100

    Some questions answered

---
+++ inheritanceQuestions.py
@@ -0,0 +1,15 @@
+# inheritance is mechanism when
+#  your create a sub class to take the methods and
+#  class variables from the super class
+
+#You create a subclass by write 'class' then the subclass's name,
+#  then in brackets you write the superclass's name
+
+#superclass is like a parent class, superclass is the nornal class before you inherit it. 
+# When you inherit the normal class it is called a 'superclass'
+
+
+
+# The term "method overriding" in Python inheritance is when you crete a method 
+# in a subclass with the same name with the method in the superclass and,
+# you make it do something else.
